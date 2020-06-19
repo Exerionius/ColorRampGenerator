8cfd91850e6e8d5a1fabf31b9565883e3e06743c
Exerionius <exerion@mail.ru>
2020-06-19 16:47:39 +0300

    Bind Enter KeyUp event to update textboxes

--- ColorRampGenerator/Windows/MainWindow.xaml.cs
+++ ColorRampGenerator/Windows/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using ColorRampGenerator.ViewModels;
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using ColorRampGenerator.ViewModels;
 
 namespace ColorRampGenerator.Windows
 {
@@ -10,6 +13,18 @@ namespace ColorRampGenerator.Windows
             InitializeComponent();
             Chart.AxisX[0].LabelFormatter = _ => string.Empty;
             Chart.AxisY[0].LabelFormatter = _ => string.Empty;
+        }
+        
+        private void TextBox_KeyEnterUpdate(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var tBox = (TextBox)sender;
+                var prop = TextBox.TextProperty;
+
+                var binding = BindingOperations.GetBindingExpression(tBox, prop);
+                binding?.UpdateSource();
+            }
         }
     }
 }
